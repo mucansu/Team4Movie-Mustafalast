e195ff1876ff6fd15c1feef0b0d637c03ac5cfc2
mucansu <mucansu@gmail.com>
2022-12-22 15:28:13 +0100

    test added

--- src/test/java/com/example/Movie/MovieApplicationTests.java
+++ src/test/java/com/example/Movie/MovieApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.Movie;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 class MovieApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testProduct() {
+		Snacks snacks = new Snacks("Djungelvrål", 15.99,"djungelvrål.jpg", "Vrålsalta lakritsdjur har varit en av Malacos favorit hos många godisälskare. Passar dig som älskar extra salt salmiak." );
+		String result = snacks.getProduct();
+		Assertions.assertEquals("Djungelvrål", result);
 	}
 
 }
